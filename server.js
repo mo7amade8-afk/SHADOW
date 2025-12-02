8170fa856d9dbe31adf916f0bd4e463b58d654a5
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-02 16:27:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const API = `https://api.telegram.org/bot${TOKEN}/`;
 
 // محتويات image.js
 const image = {
-  جلجامشة: "https://example.com/جلجامشة.jpg",
+  جلجامشة: "https://github.com/mo7amade8-afk/SHADOW/blob/1d81f6ed8f2d799f7b736549f663fc27b9874bf1/%D8%AC%D9%84%D8%AC%D8%A7%D9%85%D8%B4%D8%A9.jpg",
   dog: "https://example.com/dog.jpg"
 };
 
