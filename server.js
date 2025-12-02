415c35084c9e42e5ab9d6489b89b963ff38f6a84
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-02 20:15:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-// Data files
 import image from "./image.js";
-import textData from "./text.js";
-import video from "./video.js";
-import audio from "./audio.js";
-import file from "./file.js";
+import textData from "./text.js"; // 
 
 const app = express();
 app.use(bodyParser.json());
@@ -31,27 +28,6 @@ async function sendPhoto(chatId, url) {
   });
 }
 
-async function sendVideo(chatId, url) {
-  await axios.post(API + "sendVideo", {
-    chat_id: chatId,
-    video: url
-  });
-}
-
-async function sendAudio(chatId, url) {
-  await axios.post(API + "sendAudio", {
-    chat_id: chatId,
-    audio: url
-  });
-}
-
-async function sendFile(chatId, url) {
-  await axios.post(API + "sendDocument", {
-    chat_id: chatId,
-    document: url
-  });
-}
-
 app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 
@@ -66,12 +42,17 @@ app.post("/webhook", async (req, res) => {
       return sendMessage(chatId, "Bot is ready");
     }
 
-    if (image[userText]) return sendPhoto(chatId, image[userText]);
-    if (textData[userText]) return sendMessage(chatId, textData[userText]);
-    if (video[userText]) return sendVideo(chatId, video[userText]);
-    if (audio[userText]) return sendAudio(chatId, audio[userText]);
-    if (file[userText]) return sendFile(chatId, file[userText]);
+    // 
+    if (image[userText]) {
+      return sendPhoto(chatId, image[userText]);
+    }
 
+    // 
+    if (textData[userText]) {
+      return sendMessage(chatId, textData[userText]);
+    }
+
+    // 
     sendMessage(chatId, "Unknown command");
   } catch (e) {
     console.log("Error:", e);
