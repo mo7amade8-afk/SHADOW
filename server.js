3df747b91ab2166ed135243262e9374514c7bd7f
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-02 16:42:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const API = `https://api.telegram.org/bot${TOKEN}/`;
 
 // محتويات image.js
 const image = {
-  جلجامشة: "https://github.com/mo7amade8-afk/SHADOW/blob/1d81f6ed8f2d799f7b736549f663fc27b9874bf1/%D8%AC%D9%84%D8%AC%D8%A7%D9%85%D8%B4%D8%A9.jpg",
-  dog: "https://example.com/dog.jpg"
+  جلجامشة: "https://github.com/جلجامشة.jpg",
+  dog: "https://github.com/جلجامشة.jpg"
 };
 
 // محتويات text.js
