42754bbdf7236256814c5fc8951b39d60982a62c
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-03 02:25:30 +0100

    Update image.js

--- image.js
+++ image.js
@@ -1,12 +1,12 @@
 export default {
-  cat: [
+  جيجي: [
     "https://site/cat1.jpg",
     "https://site/cat2.jpg",
     "https://site/cat3.jpg"
   ],
 
-  dog: [
-    "https://site/dog1.jpg",
+  شيرا: [
+    "فضح_جيجي",
     "https://site/dog2.jpg",
     "https://site/dog3.jpg"
   ],
