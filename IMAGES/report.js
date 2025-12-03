84bf3169021fc1e3e4ebb51d97907cdfa0529819
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-03 13:20:01 +0100

    Update report.js

--- IMAGES/report.js
+++ IMAGES/report.js
@@ -1,1 +1,23 @@
+import fs from "fs";
+import path from "path";
 
+export function reportImage(bot, msg) {
+    const reportPath = path.resolve("./reports.json");
+
+    const reportData = {
+        user: msg.from.username,
+        userId: msg.from.id,
+        date: new Date(),
+        file: msg.text
+    };
+
+    let file = [];
+    if (fs.existsSync(reportPath)) {
+        file = JSON.parse(fs.readFileSync(reportPath));
+    }
+
+    file.push(reportData);
+    fs.writeFileSync(reportPath, JSON.stringify(file, null, 2));
+
+    bot.sendMessage(msg.chat.id, "Report submitted.");
+}
