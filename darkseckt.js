ea35e659ea98ff6098443d895ce559c632e7c455
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-03 12:30:33 +0100

    Update darkseckt.js

--- darkseckt.js
+++ darkseckt.js
@@ -3,16 +3,10 @@ import { ShadowCore } from "./SHADOW/xxx.js";
 export class DarkSystem {
   constructor() {
     console.log("[DARKSECKT] Loaded");
-    this.shadow = new ShadowCore();
+    this.core = new ShadowCore();
   }
 
   async handleMessage(data) {
-    try {
-      const result = await this.shadow.process(data);
-      return result;
-    } catch (err) {
-      console.error("[DARKSECKT ERROR]", err);
-      throw err;
-    }
+    return await this.core.process(data);
   }
 }
