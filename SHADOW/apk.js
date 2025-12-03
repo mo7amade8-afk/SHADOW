8913ffa91d3e09ffb029954310bb39138d09de92
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-03 17:44:35 +0100

    Update apk.js

--- SHADOW/apk.js
+++ SHADOW/apk.js
@@ -1,1 +1,15 @@
+import { handleAdminApk } from "../APKS/adminapk.js";
 
+export class ApkController {
+  constructor() {
+    this.admin = handleAdminApk;
+  }
+
+  async process(command) {
+    if (!command || typeof command !== "string") return null;
+
+    const normalized = command.trim().toLowerCase();
+
+    return await this.admin(normalized);
+  }
+}
